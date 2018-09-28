36d7765f82096279a0a9764f270244ca7f79003e
Low Zhi Heng <lowzhiheng@gmail.com>
2018-09-29 00:52:14 +0800

    ++ Added Bunch of tools and scripts for block building ++ Added batch renaming tool

---
+++ Assets/ZhiHeng's Playground/Scripts/BlockType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BlockType : ScriptableObject
+{
+    [SerializeField]
+    private string type;
+
+    private string GetType()
+    {
+        return type;
+    }
+}
---
+++ Assets/ZhiHeng's Playground/Scripts/ConnectionsData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BlockConnectionDataStorage : ScriptableObject
+{
+    [SerializeField]
+    private List<BlockConnectionData> blockConnectionDataList = new List<BlockConnectionData>();
+
+    public void SetConnectionDataList(List<BlockConnectionData> newData)
+    {
+        blockConnectionDataList.Clear();
+        blockConnectionDataList = newData;
+    }
+
+    public List<BlockConnectionData> GetConnectionDataList()
+    {
+        return blockConnectionDataList;
+    }
+
+    public int GetNumOfConnections()
+    {
+        return blockConnectionDataList.Count;
+    }
+}
+
+[System.Serializable]
+public class BlockConnectionData
+{
+    public string blockAName;
+    public string blockAConnection;
+    public string blockBName;
+    public string blockBConnection;
+}
