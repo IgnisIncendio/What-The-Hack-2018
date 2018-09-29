2cb0fc83eaa132827115f85d74ed512770b8e3fe
Low Zhi Heng <lowzhiheng@gmail.com>
2018-09-29 15:41:06 +0800

    ++ work on animator

---
+++ Assets/ZhiHeng's Playground/ScrewAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewAnimator : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject screw_Prefab;
+    [SerializeField]
+    private GameObject screwDriver;
+
+    [Header("Other Parameters")]
+    [SerializeField]
+    private float screwStartYPosOffset;
+    [SerializeField]
+    private float screwdriverDistAwayFromScrew;
+    [SerializeField]
+    private float screwDownSpd;
+    [SerializeField]
+    private float rotationalSpd;
+}
