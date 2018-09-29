6b185892c55ebc9053f1cc85898e9ed2a56e1cd6
Low Zhi Heng <lowzhiheng@gmail.com>
2018-09-29 17:18:22 +0800

    ++ added scriptable obj animation and stuff

--- Assets/ZhiHeng's Playground/ScrewAnimator.cs
+++ Assets/ZhiHeng's Playground/ScrewAnimator.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class ScrewAnimator : MonoBehaviour
 {
-    [Header("Prefabs")]
-    [SerializeField]
-    private GameObject screw_Prefab;
-    [SerializeField]
+    private bool runAnimation = false;
+
+    private float screwDownSpd;
+    private float rotationalSpd;
+
+    private GameObject screw;
     private GameObject screwDriver;
 
-    [Header("Other Parameters")]
-    [SerializeField]
-    private float screwStartYPosOffset;
-    [SerializeField]
-    private float screwdriverDistAwayFromScrew;
-    [SerializeField]
-    private float screwDownSpd;
-    [SerializeField]
-    private float rotationalSpd;
+    private void Update()
+    {
+        if(runAnimation && screw && screwDriver)
+        {
+            screw.transform.Translate(-transform.up * Time.deltaTime * screwDownSpd);
+            screwDriver.transform.Translate(-transform.up * Time.deltaTime * screwDownSpd);
+            screwDriver.transform.Rotate(new Vector3(0, rotationalSpd * Time.deltaTime, 0));
+
+            if(Vector3.Distance(transform.position, screw.transform.position) < 0.01f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+    public void StartAnimation(ScrewSO screwSO, ScrewDriverSO screwDriverSO)
+    {
+        runAnimation = true;
+
+        screw = Instantiate(screwSO.GetPrefab(), transform.position + new Vector3(0, screwSO.GetStartDistFromHole(), 0), transform.rotation);
+        screwDriver = Instantiate(screwDriverSO.GetPrefab(), transform.position + new Vector3(0, screwSO.GetStartDistFromHole(), 0), transform.rotation);
+        screw.transform.parent = transform;
+        screwDriver.transform.parent = transform;
+    }
 }
