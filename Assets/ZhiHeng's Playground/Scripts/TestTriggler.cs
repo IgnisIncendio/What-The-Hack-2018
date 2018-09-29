a04b24d3feac3c4bb94bce6b102ab876b01929ba
Low Zhi Heng <lowzhiheng@gmail.com>
2018-09-30 01:21:22 +0800

    Added line component

--- Assets/ZhiHeng's Playground/Scripts/TestTriggler.cs
+++ Assets/ZhiHeng's Playground/Scripts/TestTriggler.cs
@@ -6,12 +6,22 @@ public class TestTriggler : MonoBehaviour
 {
     public ScrewSO screwSO;
     public ScrewDriverSO screwDriverSO;
-
     public ScrewAnimator targetScrewA;
+
+    [Space(10)]
+    public DottedLineComponent dottedLineComponent;
+    public Vector3 point1;
+    public Vector3 point2;
 
     [ContextMenu("Trigger ScrewAnim")]
     private void TriggerScrewAnim()
     {
         targetScrewA.StartAnimation(screwSO, screwDriverSO);
+    }
+
+    [ContextMenu("Trigger DottedLine")]
+    private void TriggerDottedLine()
+    {
+        dottedLineComponent.SetPoints(point1, point2);
     }
 }
