c1ba13bd942572c00e5e703f58d12550c6dd3ad6
Vauser Von Shepherd <vauservonshepherd@yahoo.com.sg>
2018-09-29 22:03:08 +0800

    My Update
    A lotta, mostly building features

---
+++ Assets/ZhenXiong's Playground/Resources/BuildingManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    public List<SectionManager> ListOfSteps = new List<SectionManager>();
+
+    public int stepVal = 0;
+
+    private void Start()
+    {
+        Initiate();
+    }
+
+    void Initiate()
+    {
+        foreach(SectionManager section in ListOfSteps)
+        {
+            section.gameObject.SetActive(false);
+            section.m_Manager = this;
+        }
+
+        ListOfSteps[0].gameObject.SetActive(true);
+    }
+    
+    public void SetSection(int changeVal)
+    {
+        ListOfSteps[stepVal].gameObject.SetActive(false);
+
+        ++stepVal;
+        
+        ListOfSteps[stepVal].gameObject.SetActive(true);
+    }
+}
--- Assets/ZhenXiong's Playground/Resources/Parts_ScriptableObject.cs
+++ Assets/ZhenXiong's Playground/Resources/Parts_ScriptableObject.cs
@@ -2,7 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Part", menuName = "Scriptable/Part", order = 1)]
 public class Parts_ScriptableObject : ScriptableObject
 {
     public string m_PartName;
+
+    [Multiline]
+    public string m_Instruction;
+
+    public bool CheckPartCompatibility(Parts_ScriptableObject partToCompare)
+    {
+        if(m_PartName == partToCompare.m_PartName)
+        {
+            return true;
+        }
+        return false;
+    }
 }
